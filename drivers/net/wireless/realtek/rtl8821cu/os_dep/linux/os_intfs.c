5a8449b9f7fe833dd61265b9cb18a13d97d12c16
JamesAtChamSys <james.crickmere@chamsys.co.uk>
2020-06-23 15:44:57 +0100

    [ChamSys Ltd] Get RTL8821CU driver compiling and running as a built-in module on MQ50HD/MQ70HD

--- drivers/net/wireless/realtek/rtl8821cu/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/realtek/rtl8821cu/os_dep/linux/os_intfs.c
@@ -1562,9 +1562,6 @@ static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
 	#else
 	, void *accel_priv
 	#endif
-	#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
-	, select_queue_fallback_t fallback
-	#endif
 #endif
 )
 {
